c65037a235b0808b28978ff87a7150d32dd4969c
Martin <maa.velazquez@alumno.um.edu.ar>
2024-05-01 00:18:57 -0300

    commit importpersona

---
+++ importPersona.py
@@ -0,0 +1,15 @@
+from persona import Persona
+
+if __name__ == "__main__":
+
+    persona = Persona()
+    print(persona)                                          
+
+    persona = Persona()
+    print(persona)
+
+    persona = Persona()
+    print(persona)
+
+    # persona.mostrar_datos()
+    # print(persona.mostrar_datos())
